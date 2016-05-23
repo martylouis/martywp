b1ca02c22b51c3023d9953f0fbf815bcd2c4f96f
Marty Thierry <marty@martylouis.com>
2016-05-22 20:52:11 -0400

    Setup google analytics ID to be set by new ACF options field in WP Admin

--- lib/scripts.php
+++ lib/scripts.php
@@ -82,23 +82,29 @@ add_action('wp_head', '_base_jquery_local_fallback');
  *
  * Cookie domain is 'auto' configured. See: http://goo.gl/VUCHKM
  */
-function _base_google_analytics() { ?>
-<script>
-  <?php if (WP_ENV === 'production') : ?>
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-    e.src='//www.google-analytics.com/analytics.js';
-    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-  <?php else : ?>
-    function ga() {
-      console.log('GoogleAnalytics: ' + [].slice.call(arguments));
-    }
-  <?php endif; ?>
-  ga('create','<?php echo GOOGLE_ANALYTICS_ID; ?>','auto');ga('send','pageview');
+function _base_google_analytics() {
+  if (function_exists('get_field') && get_field('google_analytics_id', 'options') ) :
+    $ga_ID = get_field('google_analytics_id', 'options');
+?><script>
+<?php if (WP_ENV === 'production') : ?>
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+   function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+   e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+   e.src='//www.google-analytics.com/analytics.js';
+   r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ <?php else : ?>
+function ga() {
+  console.log('GoogleAnalytics: ' + [].slice.call(arguments));
+}
+<?php endif;?>
+ga('create','<?php echo $ga_ID; ?>','auto');ga('send','pageview');
 </script>
+<?php
+ else:
+  $ga_ID = '';
+endif;
+}
 
-<?php }
-if (GOOGLE_ANALYTICS_ID && (WP_ENV !== 'production' || !current_user_can('manage_options'))) {
+if (WP_ENV !== 'production' || !current_user_can('manage_options')) {
   add_action('wp_head', '_base_google_analytics', 20);
 }
