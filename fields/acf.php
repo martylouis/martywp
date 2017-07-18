af31e51fc9b8a66605838462e4b5b60af435f7c5
martylouis <marty@martylouis.com>
2017-07-18 18:08:30 -0500

    Update json path, add Ninja Forms integration

--- fields/acf.php
+++ fields/acf.php
@@ -5,7 +5,7 @@ if ( WP_ENV === 'development') :
   add_filter('acf/settings/show_admin', '__return_true');
 
   function _base_acf_json_save_point( $path ) {
-    $path = get_stylesheet_directory() . '/fields/acf/json';
+    $path = get_stylesheet_directory() . '/fields/json';
     return $path;
   }
 
@@ -18,10 +18,30 @@ else :
 
   function _base_acf_json_load_point( $paths ) {
     unset($paths[0]); // remove original path (optional)
-    $paths[] = get_stylesheet_directory() . '/fields/acf/json';
+    $paths[] = get_stylesheet_directory() . '/fields/json';
     return $paths;
   }
 
   add_filter('acf/settings/load_json', '_base_acf_json_load_point');
 
 endif;
+
+
+// Ninja Form Integration
+function acf_load_ninja_forms($field) {
+  // Reset Choices
+  $field['choices'] = [];
+
+  // Get Ninja Form Ids and Titles
+  $forms = Ninja_Forms()->form()->get_forms();
+
+  foreach ($forms as $form) {
+    $label = $form->get_setting('title');
+    $value = $form->get_id();
+
+    $field['choices'][$value] = $label;
+  }
+
+  return $field;
+}
+add_filter('acf/load_field/name=select_form', 'acf_load_ninja_forms');
