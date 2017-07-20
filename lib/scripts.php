41aefa32289b73989936baa41fabf75f9f2aae0a
martylouis <marty@martylouis.com>
2017-07-19 20:45:38 -0500

    Move production files to assets/dist

--- lib/scripts.php
+++ lib/scripts.php
@@ -1,59 +1,61 @@
 <?php
 
-function _base_scripts() {
-  /**
-   * The build task in Grunt renames production assets with a hash
-   * Read the asset names from assets-manifest.json
-   */
-  if (WP_ENV === 'development') {
-    $assets = [
-      'css'           => '/assets/css/main.css',
-      'js'            => '/assets/js/scripts.js',
-      'modernizr'     => '/assets/js/vendor/modernizr.min.js',
-      'jquery'        => 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'
-    ];
-  } else {
-    $get_manifest = file_get_contents(get_template_directory() . '/assets/manifest.json');
-    $manifest = json_decode($get_manifest, true);
-    $assets = [
-      'css'           => '/assets/css/main.min.css?' . $manifest['assets/css/main.min.css']['hash'],
-      'js'            => '/assets/js/scripts.min.js?' . $manifest['assets/js/scripts.min.js']['hash'],
-      'modernizr'     => '/assets/js/vendor/modernizr.min.js',
-      'jquery'        => '//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'
-    ];
-  }
+/**
+ *  Set asset path based on WP_ENV
+ */
+function _base_asset_path($name, $type) {
+  $wp_dev = (WP_ENV === 'development' ? true : false);
+  $uri = get_template_directory_uri();
+  $path = $wp_dev ? sprintf('%1$s/assets/%2$s/%3$s.%2$s', $uri, $type, $name) : sprintf('%1$s/assets/dist/%2$s/%3$s.min.%2$s', $uri, $type, $name);
+  return $path;
+}
 
-  wp_enqueue_style('base_css', get_template_directory_uri() . $assets['css'], false, null);
+
+/**
+ *  Enqueue the scripts
+ */
+function _base_scripts() {
+
+  $wp_dev = (WP_ENV === 'development' ? true : false);
+  $get_package = file_get_contents(get_template_directory() . '/package.json');
+  $pkg = json_decode($get_package, true);
+  $pkg_ver = $wp_dev ? null : $pkg['version'];
+
+  wp_enqueue_style('base_css', _base_asset_path('styles', 'css'), null, $pkg_ver, null);
 
   /**
    * jQuery is loaded using the same method from HTML5 Boilerplate:
    * Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
    * It's kept in the header instead of footer to avoid conflicts with plugins.
    */
+
+  $jquery_ver = '3.2.1';
+
   if (!is_admin() && current_theme_supports('jquery-cdn')) {
     wp_deregister_script('jquery');
-    wp_register_script('jquery', $assets['jquery'], array(), null, false);
+    wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/'. $jquery_ver . '/jquery.min.js', array(), null, false);
     add_filter('script_loader_src', '_base_jquery_local_fallback', 10, 2);
   }
+  wp_enqueue_script('jquery');
 
   if (is_single() && comments_open() && get_option('thread_comments')) {
     wp_enqueue_script('comment-reply');
   }
 
-  wp_enqueue_script('jquery');
-  wp_enqueue_script('modernizr', get_template_directory_uri() . $assets['modernizr'], array(), null, true);
-  wp_enqueue_script('base_js', get_template_directory_uri() . $assets['js'], array(), null, true);
+  wp_enqueue_script('base_js', _base_asset_path('scripts', 'js'), array(), $pkg_ver, true);
 }
 add_action('wp_enqueue_scripts', '_base_scripts', 100);
 
 
-
-// http://wordpress.stackexchange.com/a/12450
+/**
+ *  jQuery Local Fallback
+ *  @link http://wordpress.stackexchange.com/a/12450
+ */
 function _base_jquery_local_fallback($src, $handle = null) {
   static $add_jquery_fallback = false;
 
   if ($add_jquery_fallback) {
-    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/js/vendor/jquery.min.js?2.2.4"><\/script>\')</script>' . "\n";
+    echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/dist/js/vendor/jquery.min.js"><\/script>\')</script>' . "\n";
     $add_jquery_fallback = false;
   }
 
