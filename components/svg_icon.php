597e8df040f5a8eeb9d3c681e027ec9e5aaeed85
martylouis <marty@martylouis.com>
2017-07-19 20:44:19 -0500

    Update svg href

--- components/svg_icon.php
+++ components/svg_icon.php
@@ -5,7 +5,7 @@
  */
 
 function get_svg_icon($icon_name) {
-  return sprintf('<i class="icon icon-%1$s"><svg class="svg"><use xlink:href="#icon-%1$s"></svg></i>', $icon_name);
+  return sprintf('<i class="icon icon-%1$s"><svg class="svg"><use xlink:href="#svg-%1$s"></svg></i>', $icon_name);
 }
 
 function the_svg_icon($icon_name) {
