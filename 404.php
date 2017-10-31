1041f56d8ef486d9fcbe6e37ca726b4b3d449b2d
martylouis <marty@martylouis.com>
2017-10-30 20:33:05 -0500

    Update 404 layout

--- 404.php
+++ 404.php
@@ -1,18 +1,20 @@
 <?php get_template_part('components/site/header'); ?>
-<section class="content">
-  <div class="container">
-    <header class="page-header">
-      <h1 class="page-title"><?php echo _base_title(); ?></h1>
-    </header>
-    <div class="alert">
-      <?php _e('Sorry, but the page you were trying to view does not exist.', '_base'); ?>
+<div class="container">
+  <main class="site-main">
+    <div class="main-content">
+      <header class="page-header">
+        <h1 class="page-title"><?php echo _base_title(); ?></h1>
+      </header>
+      <div class="alert">
+        <?php _e('Sorry, but the page you were trying to view does not exist.', '_base'); ?>
+      </div>
+      <p><?php _e('It looks like this was the result of either:', '_base'); ?></p>
+      <ul>
+        <li><?php _e('a mistyped address', '_base'); ?></li>
+        <li><?php _e('an out-of-date link', '_base'); ?></li>
+      </ul>
+      <?php get_search_form(); ?>
     </div>
-    <p><?php _e('It looks like this was the result of either:', '_base'); ?></p>
-    <ul>
-      <li><?php _e('a mistyped address', '_base'); ?></li>
-      <li><?php _e('an out-of-date link', '_base'); ?></li>
-    </ul>
-    <?php get_search_form(); ?>
-  </div>
-</section>
+  </main>
+</div>
 <?php get_template_part('components/site/footer'); ?>
