dcc6eb21b50777d6dd5627d9647288579a64bf0d
martylouis <marty@martylouis.com>
2017-10-30 20:44:14 -0500

    Update post list, move post meta to entry-header

--- components/post/list.php
+++ components/post/list.php
@@ -1,15 +1,10 @@
 <article <?php post_class(); ?> itemscope itemtype="http://schema.org/BlogPosting">
   <?php get_template_part('components/post/thumb'); ?>
   <header class="entry-header">
-    <h2 class="entry-heading" itemprop="headline"><a itemprop="url" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2 class="entry-heading" itemprop="headline"><a itemprop="url" href="<?php the_permalink(); ?>"><?php echo the_title(); ?></a></h2>
+    <?php get_template_part('components/post/meta'); ?>
   </header>
   <div class="entry-excerpt">
     <?php the_excerpt(); ?>
   </div>
-  <footer class="entry-footer">
-    <div class="entry-read-more">
-      <a href="<?php echo the_permalink(); ?>" class="button"><?php _e('Read More &rarr;', '_base'); ?></a>
-    </div>
-    <?php get_template_part('components/post/meta'); ?>
-  </footer>
 </article>
