96ba2e07c9d3f784f9052b1178344d93283638f0
martylouis <marty@martylouis.com>
2017-10-30 20:34:55 -0500

    Update post thumb

--- components/post/thumb.php
+++ components/post/thumb.php
@@ -1,1 +1,4 @@
-<?php if (has_post_thumbnail()) printf(__('<div class="entry-thumb">%s</div>'), get_the_post_thumbnail($post->ID, 'featured_img', ['itemprop' => 'image'])); ?>
+<?php
+  if (has_post_thumbnail()) :
+    printf(__('<div class="entry-thumb">%s</div>'), get_the_post_thumbnail($post->ID, 'featured_img', ['itemprop' => 'image']));
+  endif;
